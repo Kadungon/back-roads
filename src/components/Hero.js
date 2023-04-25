02ab9f20e6e6d4382e501e4ec09870cb5a988c65
kadungon <vishnuskct@gmail.com>
2023-04-25 11:52:38 +0530

    initial commit

---
+++ src/components/Hero.js
@@ -0,0 +1,18 @@
+const Hero = () => {
+  return (
+    <div className="hero">
+      <div className="hero-banner">
+        <h1>continue exploring</h1>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequatur
+          blanditiis excepturi veniam eaque expedita rerum.
+        </p>
+        <a href="#featured" className="btn hero-btn scroll-link">
+          explore tours
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
